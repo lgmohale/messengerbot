635d22ec48ee8f1119c2f4c8b2a83f0ed5677049
Lucas George <35636264+lgmohale@users.noreply.github.com>
2019-01-14 15:27:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,46 @@
+'use strict'
+
+// Importing dependencies
 const express = require('express')
 const bodyParser = require('body-parser')
 const request=require('request')
-
 const app = express()
 
+
+// Facebook tokens
 const token = process.env.FB_VERIFY_TOKEN
 const access = process.env.FB_ACCESS_TOKEN
 
-app.set('port',(process.env.PORT || 5000))
 
-//Allows us to process the data
+// Setting server port 
+app.set('port',(process.env.PORT))
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
-//ROUTES
 
+// Setting route
 app.get('/', function(req, res){
-  res.send("Hello I am chatbot");
-})
+  res.send("Hello, I am spaceFeX chat-bot");
+});
 
-//Facebook
+
+// Creating the endpoint for the webhook
 app.get('/webhook/', function(req, res){
   if(req.query['hub.verify_token'] === token){
     res.send(req.query['hub.challenge'])
   }
   res.send("wrong token")
-})
+});
 
-//server
 
+// firing up server
 app.listen(app.get('port'), function(){
   console.log("running: port")
-})
+});
 
+
+// Adding support for GET requests to the webhook
 app.post('/webhook/', function (req, res) {
     let messaging_events = req.body.entry[0].messaging
     for (let i = 0; i < messaging_events.length; i++) {
@@ -41,29 +48,33 @@ app.post('/webhook/', function (req, res) {
       let sender = event.sender.id
       if (event.message && event.message.text) {
         let text = event.message.text
+        
+        switch(text){
+            case "hello":
+            sendTextMessage(sender, "Hello, Welcom to the spaceFeX, Please refer to spaceFeX manual to be able to chat with me about spaceEagle engine statuses");
+            break;
 
-        if(text === "engine1"){
-            sendTextMessage(sender, "engine 1 ok ")
+            case "engine1":
+            sendTextMessage(sender, "engine 1 ok ");
+            break;
+
+            case "engine2":
+            sendTextMessage(sender, "engine 2 operationg at 55% capacity ");
+            break;
+
+            case "engine3":
+            sendTextMessage(sender, "engine 3 is dead");
+            break;
+
+            case "engine4":
+            sendTextMessage(sender, "engine 4 operationg at 20% capacity");
+            break;
+
+            default:
+            sendTextMessage(sender, "Please refer to spaceFeX manual to be able to chat with me about spaceEagle engine statuses.");
         }
-        else if(text === "engine2"){
-            sendTextMessage(sender, "engine 2 operationg at 55% capacity ")
-        }
-        else if(text === "engine3"){
-            sendTextMessage(sender, "engine 3 is dead")
-        }
-        else if(text === "engine4"){
-            sendTextMessage(sender, "engine 4 operationg at 20% capacity")
-        }
-        else {
-            sendTextMessage(sender, "Please refer to spaceFeX manual to be able to chat with me about spaceEagle engine statuses.")
-        }
-        // if (text === 'Generic') {
-        //     sendGenericMessage(sender)
-        //     continue
-        // }
-        // if(text.inc)
-        // sendTextMessage(sender, "Message received: " + text.substring(0, 200))
-      }
+      };
+
       if (event.postback) {
         let text = JSON.stringify(event.postback)
         sendTextMessage(sender, "Postback: "+text.substring(0, 200), token)
@@ -71,9 +82,10 @@ app.post('/webhook/', function (req, res) {
       }
     }
     res.sendStatus(200)
-  })
+  });
 
 
+// Handling messages events
 function sendTextMessage(sender, text) {
     let messageData = { text:text }
     request({
@@ -90,54 +102,5 @@ function sendTextMessage(sender, text) {
         } else if (response.body.error) {
             console.log('Error: ', response.body.error)
         }
-    })
-}
-
-function sendGenericMessage(sender) {
-    let messageData = {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": [{
-                    "title": "First card",
-                    "subtitle": "Element #1 of an hscroll",
-                    "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-                    "buttons": [{
-                        "type": "web_url",
-                        "url": "https://www.messenger.com",
-                        "title": "web url"
-                    }, {
-                        "type": "postback",
-                        "title": "Postback",
-                        "payload": "Payload for first element in a generic bubble",
-                    }],
-                }, {
-                    "title": "Second card",
-                    "subtitle": "Element #2 of an hscroll",
-                    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-                    "buttons": [{
-                        "type": "postback",
-                        "title": "Postback",
-                        "payload": "Payload for second element in a generic bubble",
-                    }],
-                }]
-            }
-        }
-    }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token: access},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}
+    });
+};
